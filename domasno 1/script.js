d20f4ba698c1f0cc2fcc0c34d1fae7700fbddb3a
Dejans98 <spasovskid98@gmail.com>
2024-12-07 15:31:35 +0100

    domasno 1

---
+++ domasno 1/script.js
@@ -0,0 +1,39 @@
+//1
+let phones = 30;
+let pricePhones = 119.95;
+let taxRate = 5;
+
+let totalPrice = phones * pricePhones;
+let totalTaxRate = totalPrice * (taxRate / 100);
+let finalPrice = totalPrice - totalTaxRate;
+console.log(finalPrice);
+
+//2
+let oneMinut = 3;
+let seconds = 60;
+let result = oneMinut * seconds;
+console.log(result);
+
+//3
+let number = 4;
+let newNumber = number + 1;
+console.log(newNumber);
+
+//4
+
+let base = 5;
+let height = 10;
+let area = (1 / 2) * base * height;
+console.log(area);
+
+//5
+let legnth = 6;
+let widht = 4;
+let perimetar = 2 * legnth + 2 * widht;
+console.log(perimetar);
+
+//6
+let valeOfP = 3.14;
+let radius = 6;
+let areOfCircle = radius * radius * valeOfP;
+console.log(areOfCircle);
